c4c113fba1b1e1c59d667ec106c17b30ae46db2d
WiegerWolf <124579865+WiegerWolf@users.noreply.github.com>
2024-05-07 00:18:17 +0000

    zipEntry.getData

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # chatgpt-to-lobe-chat-migration
 
 1. Export chat history from chatgpt.com: Profile -> Settings -> Data Controls -> Export Data -> Export
-2. Data will be sent to your email; download and unzip into `input` folder
+2. Data will be sent to your email; download and put downloaded zip file into `input` folder
 3. Run `deno run --allow-all main.ts`
 4. Chat history will be converted to lobe.ai format and saved in `output` folder
 5. Profile -> Import Configuration
--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { extname } from "https://deno.land/std@0.224.0/path/mod.ts";
+import * as zip from "jsr:@zip-js/zip-js";
+
+const {
+  INPUT_DIR = './input',
+  OUTPUT_DIR = './output',
+  LOOK_FOR_FILETYPE = '.zip',
+  CONVERSATIONS_FILE = 'conversations.json',
+} = Deno.env.toObject();
+
+async function main() {
+  for await (const dirEntry of Deno.readDir(INPUT_DIR)) {
+    if (!dirEntry.isFile) {
+      continue;
+    }
+    const inputFilePath = `${INPUT_DIR}/${dirEntry.name}`;
+    const inputFileExtension = extname(dirEntry.name);
+    if (inputFileExtension=== LOOK_FOR_FILETYPE) {
+      const zipFile = await Deno.open(inputFilePath);
+      const zipReader = new zip.ZipReader(zipFile);
+      const zipEntries = await zipReader.getEntries();
+      for await (const zipEntry of zipEntries) {
+        if (!zipEntry||!zipEntry.getData) {
+          continue;
+        }
+        if (zipEntry.filename !== CONVERSATIONS_FILE) {
+          continue;
+        }
+        const zipTextWriter = new zip.TextWriter();
+        const zipEntryData = await zipEntry.getData(zipTextWriter)
+        try {
+          const json = JSON.parse(zipEntryData);
+          // TODO: write to output file after parsing and converting to lobe format
+        } catch (e) {
+          console.error(e);
+          continue;
+        }
+      }
+      zipFile.close();
+    }
+  }
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+main();
